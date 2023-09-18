764f5e9f9a62794b36378ea13bba433f7d585646
Ethryan-ramart <lioramartoficial@gmail.com>
2023-09-18 01:09:46 -0600

    hotfix/switch-box-hover-delete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,11 +9,11 @@ export default function Home() {
   const theme = useTheme();
   return (
     <div id="body" className="light-mode">
-      <div className="grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20  md:p-5">
+      <div className="bounce-animation grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20  md:p-5">
         <div className="relative col-span-3 aspect-square md:aspect-auto md:row-span-2 bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl overflow-hidden">
           <PresentationCard />
         </div>
-        <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm cursor-pointer hover:scale-[103%] transition duration-300 ease-in-out">
+        <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm ">
           <SwitchTheme theme={theme} />
         </div>
         <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm"></div>
@@ -27,7 +27,7 @@ export default function Home() {
         <div className="flex flex-col text-white rounded-3xl flex relative overflow-hidden aspect-square shadow-sm bg-[#ff4848d3] p-4 justify-center items-center">
           <AgeCard />
         </div>
-        <a className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden hover:scale-[103%] transition duration-500 ease-in-out bg-[#1c053a9c]"></a>
+        <a className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden cursor-pointer hover:scale-[103%] transition duration-500 ease-in-out bg-[#1c053a9c]"></a>
       </div>
     </div>
   );
--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { dark } from "@mui/material/styles/createPalette";
+import { useEffect, useState } from "react";
 
 export default function useTheme() {
-  const [darkMode, setDarkMode] = useState(false);
-
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("theme")=== "dark");
+  
   const toggleTheme = () => {
-    // document.body.className = isBackground1 ? 'background1' : 'background2';
     const htmlElement = document.querySelector("#body");
-    const actionLight = htmlElement.classList.contains("light-mode") ? "remove" : "add";
-    htmlElement.classList[actionLight]("light-mode");
-    const actionDark = htmlElement.classList.contains("dark") ? "remove" : "add";
-    htmlElement.classList[actionDark]("dark");
+    if (darkMode) {
+      htmlElement.classList["remove"]("dark");
+      htmlElement.classList["add"]("light-mode");
+    } else {
+      htmlElement.classList["remove"]("light-mode");
+      htmlElement.classList["add"]("dark");
+    }
+
+    const newTheme = darkMode ? "light" : "dark";
+    localStorage.setItem("theme", newTheme);
+  
     setDarkMode(!darkMode);
   };
 
+  useEffect(() => {
+    const htmlElement = document.querySelector("#body");
+    const theme = localStorage.getItem("theme");
+    setDarkMode(theme === "dark");
+    if (theme === "light") {
+      htmlElement.classList.add("light-mode");
+    } else if (theme === "dark") {
+      htmlElement.classList.add("dark");
+    }
+  }, [])
   return { darkMode, toggleTheme };
 }

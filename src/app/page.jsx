dabbcaef1dd636abf74844a50b83395abef64e6d
Leonel Ramirez <lioramartoficial@gmail.com>
2023-09-26 08:46:45 -0600

    hotfix/noise-effect-fullscreen-fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,27 +11,29 @@ export default function Home() {
   const theme = useTheme();
   return (
     <div id="body" className="light">
-      <div className="bounce-animation grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20  md:p-5">
-        <div className="relative col-span-3 aspect-square md:aspect-auto md:row-span-2 light:bg-[#6e5d83] dark:bg-[#af72ff56] rounded-3xl overflow-hidden">
-          <PresentationCard />
+      <div className="noise">
+        <div className="bounce-animation grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 my-5 container mx-auto xl:px-20  md:p-5">
+          <div className="relative col-span-3 aspect-square md:aspect-auto md:row-span-2 light:bg-[#6e5d83] dark:bg-[#af72ff56] rounded-3xl overflow-hidden">
+            <PresentationCard />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm ">
+            <SwitchTheme theme={theme} />
+          </div>
+          <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm">
+            <LenguajesCard />
+          </div>
+          <a
+            href="https://www.linkedin.com/in/leonelramart/"
+            target="_blank"
+            className="flex flex-col justify-center items-center bg-[#1178ff9c] dark:bg-[#49a7ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm cursor-pointer hover:scale-[103%] transition duration-300 ease-in-out"
+          >
+            <LinkedIn className="icono" />
+          </a>
+          <div className="flex flex-col text-white rounded-3xl flex relative overflow-hidden aspect-square shadow-sm bg-[#ff4848d3] p-4 justify-center items-center">
+            <AgeCard />
+          </div>
+          <a className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden cursor-pointer hover:scale-[103%] transition duration-500 ease-in-out bg-[#1c053a9c]"></a>
         </div>
-        <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm ">
-          <SwitchTheme theme={theme} />
-        </div>
-        <div className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm">
-          <LenguajesCard />
-        </div>
-        <a
-          href="https://www.linkedin.com/in/leonelramart/"
-          target="_blank"
-          className="flex flex-col justify-center items-center bg-[#1178ff9c] dark:bg-[#49a7ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm cursor-pointer hover:scale-[103%] transition duration-300 ease-in-out"
-        >
-          <LinkedIn className="icono"/>
-        </a>
-        <div className="flex flex-col text-white rounded-3xl flex relative overflow-hidden aspect-square shadow-sm bg-[#ff4848d3] p-4 justify-center items-center">
-          <AgeCard />
-        </div>
-        <a className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden cursor-pointer hover:scale-[103%] transition duration-500 ease-in-out bg-[#1c053a9c]"></a>
       </div>
     </div>
   );
